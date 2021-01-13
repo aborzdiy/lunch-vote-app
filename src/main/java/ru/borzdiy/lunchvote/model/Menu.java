b1a79c41a2e9a4057de492f3e7b7d62ac86d2077
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-13 14:18:52 +0300

    add menu entity

---
+++ src/main/java/ru/borzdiy/lunchvote/model/Menu.java
@@ -0,0 +1,87 @@
+package ru.borzdiy.lunchvote.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "menus")
+public class Menu extends AbstractBaseEntity{
+
+    @Column(name = "MENU_DATE", nullable = false)
+    LocalDate menuDate;
+
+    @Column(name = "DISH", nullable = false)
+    @NotBlank
+    private String dish;
+
+    @NotEmpty
+    @Positive
+    @Column(name = "PRICE", nullable = false)
+    private Integer price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RESTAURANT_ID", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonBackReference
+    private Restaurant restaurant;
+
+    public Menu() {
+    }
+
+    public Menu(LocalDate menuDate, @NotBlank String dish, @NotEmpty @Positive Integer price, Restaurant restaurant) {
+        this.menuDate = menuDate;
+        this.dish = dish;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public LocalDate getMenuDate() {
+        return menuDate;
+    }
+
+    public void setMenuDate(LocalDate menuDate) {
+        this.menuDate = menuDate;
+    }
+
+    public String getDish() {
+        return dish;
+    }
+
+    public void setDish(String dish) {
+        this.dish = dish;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuDate=" + menuDate +
+                ", dish='" + dish + '\'' +
+                ", price=" + price +
+                ", restaurant=" + restaurant +
+                '}';
+    }
+}
