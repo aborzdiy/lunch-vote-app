61be1d6f1f4a082acb48f877374036ed5c4c0abc
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-13 04:55:11 +0300

    add RestaurantController methods

--- src/main/java/ru/borzdiy/lunchvote/controller/AbstractRestaurantController.java
+++ src/main/java/ru/borzdiy/lunchvote/controller/AbstractRestaurantController.java
@@ -1,5 +1,24 @@
 package ru.borzdiy.lunchvote.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.borzdiy.lunchvote.model.Restaurant;
+import ru.borzdiy.lunchvote.service.RestaurantService;
+
+import java.util.List;
+
 public class AbstractRestaurantController extends AbstractController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    public List<Restaurant> getAll() {
+        log.info("get all");
+        return restaurantService.getAll();
+    }
+
+    public Restaurant get(int id) {
+        log.info("get with id={}", id);
+        return restaurantService.get(id);
+    }
 
 }
--- src/main/java/ru/borzdiy/lunchvote/controller/RestaurantController.java
+++ src/main/java/ru/borzdiy/lunchvote/controller/RestaurantController.java
@@ -1,11 +1,72 @@
 package ru.borzdiy.lunchvote.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.borzdiy.lunchvote.model.Restaurant;
+
+import java.net.URI;
+import java.util.List;
+
+import static ru.borzdiy.lunchvote.util.ValidationUtil.assureIdConsistent;
+import static ru.borzdiy.lunchvote.util.ValidationUtil.checkNew;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestaurantController extends AbstractRestaurantController {
+    static final String ADMIN_REST_URL = "/rest/admin/restaurants";
+    static final String USER_REST_URL = "/rest/restaurants";
+
+    @Override
+    @GetMapping({ADMIN_REST_URL, USER_REST_URL})
+    public List<Restaurant> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @GetMapping({ADMIN_REST_URL + "/{id}", USER_REST_URL + "/{id}"})
+    public Restaurant get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @PostMapping(path = ADMIN_REST_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        log.info("create {}", restaurant);
+        checkNew(restaurant);
+        Restaurant created = restaurantService.create(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(ADMIN_REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @DeleteMapping(ADMIN_REST_URL + "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        log.info("delete {}", id);
+        restaurantService.delete(id);
+    }
+
+    @PutMapping(ADMIN_REST_URL + "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Restaurant restaurant, @PathVariable int id) throws BindException {
+        validateBeforeUpdate(restaurant, id);
+        log.info("update {} with id={}", restaurant, id);
+        restaurantService.update(restaurant);
+    }
+
+    protected void validateBeforeUpdate(Restaurant restaurant, int id) throws BindException {
+        assureIdConsistent(restaurant, id);
+        DataBinder binder = new DataBinder(restaurant);
+//        binder.addValidators(emailValidator, validator);
+//        binder.validate(View.Web.class);
+        if (binder.getBindingResult().hasErrors()) {
+            throw new BindException(binder.getBindingResult());
+        }
+    }
 
 }
