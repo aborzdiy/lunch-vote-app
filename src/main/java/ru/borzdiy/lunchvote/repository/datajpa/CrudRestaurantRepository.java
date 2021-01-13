e5cbefccc398a54cabbf6a1fbd4730393aff7361
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-13 04:09:40 +0300

    add restaurant repository

---
+++ src/main/java/ru/borzdiy/lunchvote/repository/datajpa/CrudRestaurantRepository.java
@@ -0,0 +1,18 @@
+package ru.borzdiy.lunchvote.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.borzdiy.lunchvote.model.Restaurant;
+
+@Transactional(readOnly = true)
+public interface CrudRestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
+
+}
---
+++ src/main/java/ru/borzdiy/lunchvote/repository/datajpa/DataJpaRestaurantRepository.java
@@ -0,0 +1,35 @@
+package ru.borzdiy.lunchvote.repository.datajpa;
+
+import ru.borzdiy.lunchvote.model.Restaurant;
+import ru.borzdiy.lunchvote.repository.RestaurantRepository;
+
+import java.util.List;
+
+public class DataJpaRestaurantRepository implements RestaurantRepository {
+
+    private final CrudRestaurantRepository restaurantRepository;
+
+    public DataJpaRestaurantRepository(CrudRestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public Restaurant save(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return restaurantRepository.delete(id) != 0;
+    }
+
+    @Override
+    public Restaurant get(int id) {
+        return restaurantRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        return restaurantRepository.findAll();
+    }
+}
