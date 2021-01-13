e866892119c34b767f670b1452d7bea46fe45c43
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-13 04:52:42 +0300

    add validation util

---
+++ src/main/java/ru/borzdiy/lunchvote/util/ValidationUtil.java
@@ -0,0 +1,44 @@
+package ru.borzdiy.lunchvote.util;
+
+import ru.borzdiy.lunchvote.model.HasId;
+
+public class ValidationUtil {
+
+    private ValidationUtil() {
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(HasId bean, int id) {
+//      conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.id() != id) {
+            throw new IllegalRequestDataException(bean + " must be with id=" + id);
+        }
+    }
+}
