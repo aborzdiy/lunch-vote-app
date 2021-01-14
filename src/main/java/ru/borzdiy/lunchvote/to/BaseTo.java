add7458f7c5a363a0b731fa6e84fc27b04577d75
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-14 13:22:38 +0300

    add TO objects

---
+++ src/main/java/ru/borzdiy/lunchvote/to/BaseTo.java
@@ -0,0 +1,24 @@
+package ru.borzdiy.lunchvote.to;
+
+import ru.borzdiy.lunchvote.model.HasId;
+
+public abstract class BaseTo implements HasId {
+    protected Integer id;
+
+    public BaseTo() {
+    }
+
+    public BaseTo(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
