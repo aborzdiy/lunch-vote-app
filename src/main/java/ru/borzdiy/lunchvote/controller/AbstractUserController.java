e3aa68983b5a326722b3537f8bef130f26eff6bc
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-14 15:17:45 +0300

    add user controller

---
+++ src/main/java/ru/borzdiy/lunchvote/controller/AbstractUserController.java
@@ -0,0 +1,92 @@
+package ru.borzdiy.lunchvote.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.BindException;
+import org.springframework.validation.DataBinder;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import ru.borzdiy.lunchvote.model.HasId;
+import ru.borzdiy.lunchvote.model.User;
+import ru.borzdiy.lunchvote.service.UserService;
+import ru.borzdiy.lunchvote.to.UserTo;
+import ru.borzdiy.lunchvote.util.UserUtil;
+import ru.borzdiy.lunchvote.validators.UniqueMailValidator;
+
+import java.util.List;
+
+import static ru.borzdiy.lunchvote.util.ValidationUtil.assureIdConsistent;
+import static ru.borzdiy.lunchvote.util.ValidationUtil.checkNew;
+
+public abstract class AbstractUserController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    protected UserService service;
+
+    @Autowired
+    private UniqueMailValidator emailValidator;
+
+    @Autowired
+    @Qualifier("defaultValidator")
+    private Validator validator;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(emailValidator);
+    }
+
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public User create(UserTo userTo) {
+        log.info("create from to {}", userTo);
+        return create(UserUtil.createNewFromTo(userTo));
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        return service.create(user);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(UserTo userTo, int id) {
+        log.info("update {} with id={}", userTo, id);
+        service.update(userTo);
+    }
+
+    public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        return service.getByEmail(email);
+    }
+
+    public void enable(int id, boolean enabled) {
+        log.info(enabled ? "enable {}" : "disable {}", id);
+        service.enable(id, enabled);
+    }
+
+    protected void validateBeforeUpdate(HasId user, int id) throws BindException {
+        assureIdConsistent(user, id);
+        DataBinder binder = new DataBinder(user);
+        binder.addValidators(emailValidator, validator);
+        // TODO: 14.01.2021 validate group  binder.validate(View.Web.class);
+        if (binder.getBindingResult().hasErrors()) {
+            throw new BindException(binder.getBindingResult());
+        }
+    }
+}
---
+++ src/main/java/ru/borzdiy/lunchvote/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.borzdiy.lunchvote.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.borzdiy.lunchvote.model.User;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = UserController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController extends AbstractUserController {
+    static final String REST_URL = "/rest/admin/users";
+
+    @Override
+    @GetMapping
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public User get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@Validated @RequestBody User user) {
+        User created = super.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user, @PathVariable int id) throws BindException {
+        validateBeforeUpdate(user, id);
+        log.info("update {} with id={}", user, id);
+        service.update(user);
+    }
+
+    @Override
+    @GetMapping("/by")
+    public User getByMail(@RequestParam String email) {
+        return super.getByMail(email);
+    }
+
+    @Override
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable int id, @RequestParam boolean enabled) {
+        super.enable(id, enabled);
+    }
+
+}
