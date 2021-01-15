49b38cb8ce6601efc452f46f553ac4942239c407
Aleksandr Borzdiy <aborzdiy@gmail.com>
2021-01-15 13:58:37 +0300

    delete RestaurantAdminController and test fix maven-surefire-plugin encoding code changes update DTO converting to entity delete menu controller

--- src/main/java/ru/borzdiy/lunchvote/util/RestarauntUtil.java
+++ src/main/java/ru/borzdiy/lunchvote/util/RestarauntUtil.java
@@ -6,15 +6,16 @@ import ru.borzdiy.lunchvote.to.RestaurantTo;
 public class RestarauntUtil {
 
     public static Restaurant createNewFromTo(RestaurantTo userTo) {
-        return new Restaurant(null, userTo.getName());
+        return new Restaurant(null, userTo.getName(), userTo.getMenu());
     }
 
     public static RestaurantTo asTo(Restaurant restaurant) {
-        return new RestaurantTo(restaurant.getId(), restaurant.getName());
+        return new RestaurantTo(restaurant.getId(), restaurant.getName(), restaurant.getMenu());
     }
 
     public static Restaurant updateFromTo(Restaurant restaurant, RestaurantTo restaurantTo) {
         restaurant.setName(restaurantTo.getName() == null ? restaurant.getName() : restaurantTo.getName());
+        restaurant.setMenu(restaurantTo.getMenu() == null ? restaurant.getMenu() : restaurantTo.getMenu());
         return restaurant;
     }
 
